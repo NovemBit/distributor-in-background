8740ac49acbc09395d942a1ee43fe241efab4d26
avag <avag.novembit@gmail.com>
2019-07-15 16:27:51 +0400

    Add 'wp task manager' integration

--- hooks.php
+++ hooks.php
@@ -9,13 +9,13 @@ function setup() {
 	add_action(
 		'init',
 		function () {
-			add_filter( 'dt_send_notification_allow_in_background', __NAMESPACE__ . '\schedule_send_notifications', 10, 2 );
-			add_filter( 'dt_push_allow_in_background', __NAMESPACE__ . '\schedule_push_action', 10, 2 );
-			add_filter( 'dt_clone_fix_allow_in_background', __NAMESPACE__ . '\schedule_clone_fix', 10, 3 );
+			add_filter( 'dt_allow_send_notifications', __NAMESPACE__ . '\schedule_send_notifications', 10, 2 );
+			add_filter( 'dt_allow_push', __NAMESPACE__ . '\schedule_push_action', 10, 2 );
+			add_filter( 'dt_allow_clone_fix', __NAMESPACE__ . '\schedule_clone_fix', 10, 3 );
 			add_filter( 'dt_successfully_distributed_message', __NAMESPACE__ . '\change_successfully_distributed_message', 10, 1 );
-			add_action( 'dt_redistribute_posts_hook', __NAMESPACE__ . '\redistribute_posts' );
-			add_action( 'dt_push_posts_hook', __NAMESPACE__ . '\push_action' );
-			add_action( 'dt_clone_fix_hook', __NAMESPACE__ . '\clone_fix' );
+			add_action( 'dt_redistribute_posts_hook', __NAMESPACE__ . '\redistribute_posts', 10, 1 );
+			add_action( 'dt_push_posts_hook', __NAMESPACE__ . '\push_action', 10, 1 );
+			add_action( 'dt_clone_fix_hook', __NAMESPACE__ . '\clone_fix', 10, 2 );
 		}
 	);
 }
@@ -29,47 +29,54 @@ function setup() {
  * @return bool
  */
 function schedule_send_notifications( $send_notification_in_bg, $post_id ) {
-	// todo implement via 'wp-task-manager'
-	if ( ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) || wp_is_post_revision( $post_id ) ) {
-		return false;
+	// todo check 'wp-task-manager' implementation
+	if ( is_btm_active() ) {
+		$btm_task         = new \BTM_Task( 'send_notification_in_bg', [ $post_id ], 1 );
+		$btm_task_manager = BTM_Task_Manager::get_instance()->register_task( $btm_task, [ $post_id ] );
+	} else {
+		update_post_meta( $post_id, 'dt_redistribute_post', 'yes' );
+		if ( ! wp_next_scheduled( 'dt_redistribute_posts_hook' ) ) {
+			wp_schedule_single_event( time(), 'dt_redistribute_posts_hook', [ $post_id ] );
+		}
 	}
 
-	update_post_meta( $post_id, 'dt_redistribute_post', 'yes' );
-	if ( ! wp_next_scheduled( 'dt_redistribute_posts_hook' ) ) {
-		wp_schedule_single_event( time(), 'dt_redistribute_posts_hook' );
-	}
-
-	return true;
+	return false;
 }
 
 /**
  * Schedule post push
  *
- * @param bool $push_in_bg
+ * @param bool  $push_in_bg
  * @param array $params
  *
  * @return bool
  */
 function schedule_push_action( $push_in_bg, $params ) {
-	// todo implement via 'wp-task-manager'
-	if ( ! wp_next_scheduled( 'dt_push_posts_hook' ) ) {
-		wp_schedule_single_event( time(), 'dt_push_posts_hook' );
+	// todo check 'wp-task-manager' implementation
+	if ( is_btm_active() ) {
+		$btm_task         = new \BTM_Task( 'send_notification_in_bg', [ $params ], 1 );
+		$btm_task_manager = BTM_Task_Manager::get_instance()->register_task( $btm_task, [ $params ] );
+	} elseif ( ! wp_next_scheduled( 'dt_push_posts_hook' ) ) {
+		wp_schedule_single_event( time(), 'dt_push_posts_hook', [ $params ] );
 	}
 
-	return true;
+	return false;
 }
 
 /**
- * @param bool $clone_fix_in_bg
- * @param array $posts
+ * @param bool   $clone_fix_in_bg
+ * @param array  $posts
  * @param string $connection_id
  *
  * @return bool
  */
 function schedule_clone_fix( $clone_fix_in_bg, $posts, $connection_id ) {
-	// todo implement via 'wp-task-manager'
-	if ( ! wp_next_scheduled( 'dt_clone_fix_hook' ) ) {
-		wp_schedule_single_event( time(), 'dt_clone_fix_hook' );
+	// todo check 'wp-task-manager' implementation
+	if ( is_btm_active() ) {
+		$btm_task         = new \BTM_Task( 'send_notification_in_bg', [ $posts, $connection_id ], 1 );
+		$btm_task_manager = BTM_Task_Manager::get_instance()->register_task( $btm_task, [ $posts, $connection_id ] );
+	} elseif ( ! wp_next_scheduled( 'dt_clone_fix_hook' ) ) {
+		wp_schedule_single_event( time(), 'dt_clone_fix_hook', [ $posts, $connection_id ] );
 	}
 
 	return true;
@@ -78,7 +85,7 @@ function schedule_clone_fix( $clone_fix_in_bg, $posts, $connection_id ) {
 /**
  * Perform posts redistribution
  */
-function redistribute_posts() {
+function redistribute_posts( $post_id ) {
 	$query = new \WP_Query(
 		array(
 			'post_type'      => get_distributable_custom_post_types(),
@@ -132,11 +139,12 @@ function push_action( $params ) {
 /**
  * Clone fix action callback
  *
- * @param array $params
+ * @param array  $posts
+ * @param string $connection_id
  */
-function clone_fix( $params ) {
-	if( function_exists( '\DT\NbAddon\CloneFix\Hub\push_post_data' ) ) {
-		\DT\NbAddon\CloneFix\Hub\push_post_data( $params['posts'], $params['connection_id'] );
+function clone_fix( $posts, $connection_id ) {
+	if ( function_exists( '\DT\NbAddon\CloneFix\Hub\push_post_data' ) ) {
+		\DT\NbAddon\CloneFix\Hub\push_post_data( $posts, $connection_id );
 	}
 }
 
@@ -149,4 +157,13 @@ function clone_fix( $params ) {
  */
 function change_successfully_distributed_message( $message ) {
 	return esc_html__( 'Post scheduled.', 'distributor' );
+}
+
+/**
+ * Helper function to determine whether 'wp task manager' plug-in is active or not
+ *
+ * @return bool
+ */
+function is_btm_active() {
+	return class_exists( 'BTM_Plugin' );
 }
