75ae57cb1c2c9f77eb9ba316401c76a224846937
ahovhannissian <arman.hovhannisyan997@gmail.com>
2020-05-20 18:25:54 +0400

    Handle comments delete in background

--- integrations/comments.php
+++ integrations/comments.php
@@ -16,9 +16,12 @@ function setup() {
 		function () {
 			add_filter( 'dt_allow_comments_update', __NAMESPACE__ . '\schedule_comments_update', 10, 3 );
 			add_filter( 'dt_allow_comments_initial_push', __NAMESPACE__ . '\schedule_comments_insert', 10, 5 );
+			add_filter( 'dt_allow_comments_delete', __NAMESPACE__ . '\schedule_comments_delete', 10, 2 );
+
 			if ( \DT\NbAddon\DTInBackground\Helpers\is_btm_active() ) {
 				add_filter( \BTM_Plugin_Options::get_instance()->get_task_filter_name_prefix() . 'comments_update_in_bg', __NAMESPACE__ . '\bg_comments_update', 10, 3 );
 				add_filter( \BTM_Plugin_Options::get_instance()->get_task_filter_name_prefix() . 'comments_insert_in_bg', __NAMESPACE__ . '\bg_comments_insert', 10, 3 );
+				add_filter( \BTM_Plugin_Options::get_instance()->get_task_filter_name_prefix() . 'comments_delete_in_bg', __NAMESPACE__ . '\bg_comments_delete', 10, 3 );
 			}
 		}
 	);
@@ -58,6 +61,26 @@ function schedule_comments_update( $comment_processing_allowed, $parent_post_id,
 	if ( \DT\NbAddon\DTInBackground\Helpers\is_btm_active() ) {
 		$btm_task     = new \BTM_Task( 'comments_update_in_bg', [ $parent_post_id ], 10 );
 		$btm_bulk_arg = new \BTM_Task_Bulk_Argument( [ $comment_id ], -10 );
+		\BTM_Task_Manager::get_instance()->register_task( $btm_task, [ $btm_bulk_arg ] );
+
+		return false;
+	}
+
+	return true;
+}
+
+/**
+ * Schedule comment delete
+ *
+ * @param bool $comment_processing_allowed
+ * @param \WP_Comment $comment
+ *
+ * @return bool
+ */
+function schedule_comments_delete( $comment_processing_allowed, $comment ) {
+	if ( \DT\NbAddon\DTInBackground\Helpers\is_btm_active() ) {
+		$btm_task     = new \BTM_Task( 'comments_delete_in_bg', [ $comment->comment_post_ID ], 10 );
+		$btm_bulk_arg = new \BTM_Task_Bulk_Argument( [ $comment->comment_ID ], -10 );
 		\BTM_Task_Manager::get_instance()->register_task( $btm_task, [ $btm_bulk_arg ] );
 
 		return false;
@@ -153,4 +176,35 @@ function variation_update( $parent_post_id, $variation_id ) {
 	if ( function_exists( '\DT\NbAddon\Comments\Hub\handle_update' ) ) {
 		return \DT\NbAddon\Comments\Hub\handle_update( $parent_post_id, $variation_id );
 	}
+}
+
+/**
+ * Process scheduled delete
+ *
+ * @param \BTM_Task_Run_Filter_Log $task_run_filter_log
+ * @param array $callback_args
+ * @param \BTM_Task_Bulk_Argument[] $bulk_args
+ *
+ * @return \BTM_Task_Run_Filter_Log
+ */
+function bg_comments_delete( \BTM_Task_Run_Filter_Log $task_run_filter_log, array $callback_args, array $bulk_args ) {
+	$post_id = $callback_args[0];
+	$comments       = [];
+
+	foreach ( $bulk_args as $comment_arg ) {
+		$comments[] = $comment_arg->get_callback_arguments()[0];
+	}
+
+	$responses = \DT\NbAddon\Comments\Hub\handle_delete( $post_id, $comments );
+	$is_failed = false;
+	\DT\NbAddon\DTInBackground\Helpers\add_btm_logs( $post_id, $responses, $task_run_filter_log, $is_failed );
+
+	if ( $is_failed ) {
+		$task_run_filter_log->set_failed( true );
+		$task_run_filter_log->set_bulk_fails( $bulk_args );
+	} else {
+		$task_run_filter_log->set_failed( false );
+	}
+
+	return $task_run_filter_log;
 }
